c7c8378ad320a703c3d464dcb9a3c53043d40cee
PurpleDrip <gurupreethnagesha@gmail.com>
2025-03-04 07:48:19 +0530

    Added home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,131 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import CreateTree from '@/components/local/CreateTree';
+import Btn from '@/components/local/Button';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Search } from 'lucide-react';
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useRouter } from 'next/navigation';
+import { addTree, logout as logoutAction, registered } from "@/redux/userSlice";
+import { checkForCookies, logoutUser } from './api/auth';
+import { AxiosError } from 'axios';
+
+interface ErrorResponse {
+  message: string;
+}
+
+const Page = () => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const [openDialog, setOpenDialog] = useState(false);
+  const isRegistered = useSelector((state: { isRegistered: boolean }) => state.isRegistered);
+  const treeId=useSelector((state:{treeId:string })=>state.treeId);
+  const treeName=useSelector((state:{treeName:string})=>state.treeName)
+
+  const createTreeHandler = () => {
+    if (isRegistered) {
+      setOpenDialog(prev => !prev);
+    } else {
+      router.push("/register"); 
+    }
+  };
+
+  const handleLogout = async () => {
+    dispatch(logoutAction());
+    await logoutUser();
+    router.push("/login"); 
+  };
+
+  useEffect(() => {
+    const checkCookies = async () => {
+      try{
+        const res=await checkForCookies(); 
+        console.log(res);
+        dispatch(registered(true));
+        dispatch(addTree(res.data.data))
+      }catch(e){
+        const err=(e as AxiosError<ErrorResponse>).response?.data?.message;
+      }
+    };
+    checkCookies();
+  }, [])
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="h-screen p-8">
+      <section className="text-4xl flex items-center justify-between">
+        <div className="text-[#00FF00]">FamTree</div>
+        <div>
+          <form className="border p-1 rounded-full flex gap-1">
+            <Input className="rounded-full border-0 outline-0" placeholder="Search a FamTree" />
+            <button type="submit" className="flex items-center justify-center text-sm px-2">
+              <Search />
+            </button>
+          </form>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="flex gap-4 items-center justify-center">
+          {isRegistered ? (
+            <>
+              <Btn onClick={() => router.push("/about-us")}>About Us</Btn>
+              {treeId? 
+              <>
+                <Btn onClick={() => router.push(`/tree/${treeName}`)}>View Tree</Btn>
+              </> : 
+              <>
+                <Btn onClick={createTreeHandler}>Create Tree</Btn>
+              </>}
+              <button
+                onClick={handleLogout}
+                className="cursor-pointer text-sm px-4 py-2 rounded-full text-[#FF0000] bg-[#ff000018] border border-[#ff000018] hover:border-[#ff0000]"
+              >
+                Log Out
+              </button>
+            </>
+          ) : (
+            <>
+              <Btn onClick={() => router.push("/login")}>Sign In</Btn>
+              <Btn onClick={() => router.push("/register")}>Register</Btn>
+            </>
+          )}
+        </div>
+      </section>
+      <section className="h-[80vh] m-12 flex items-center justify-between">
+        <div className="w-1/2 flex flex-col gap-4 items-center">
+          <h1 className="text-5xl font-semibold">
+            Welcome to Fam Tree – Where Your Family's Story Comes to Life
+          </h1>
+          <p className="text-gray-300">
+            Preserve your family's legacy in a beautiful, interactive family tree. Add photos, stories, and
+            connections to create a living digital archive that can be shared with generations to come.
+          </p>
+          <p className="text-gray-300">
+            Easily add family members, upload cherished photos, and watch your family's history unfold in an
+            elegant, visual format. Whether you're documenting your immediate family or tracing your ancestry back
+            through generations, Fam Tree gives you the tools to capture and celebrate your unique family story.
+          </p>
+          <h2 className="text-[#00FF00] text-3xl text-center font-thin mt-16">
+            Start building your family tree today and create a lasting gift for your loved ones.
+          </h2>
+          {treeId ? (
+            <Button className="mt-2" onClick={() => router.push(`/tree/${treeName}`)}>
+              View Tree
+            </Button>
+          ) : (
+            <Button className="mt-2" onClick={createTreeHandler}>
+              Create Tree
+            </Button>
+          )}
+        </div>
+        <div className="border">
+          <Image src="/Images/hero_image1.jpeg" width="450" height="100" alt="Image of a family" />
+        </div>
+      </section>
+      <CreateTree open={openDialog} onOpenChange={setOpenDialog} />
     </div>
   );
-}
+};
+
+export default Page;
