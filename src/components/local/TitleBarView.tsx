9d7630ba8687007af61eee521db061a0f4f6f0f0
PurpleDrip <gurupreethnagesha@gmail.com>
2025-03-18 20:30:25 +0530

    Add TitleBarView component for enhanced tree view UI and refactor page to integrate it. Remove unused Redux dependencies and streamline node formatting logic in ViewTree component.

--- src/components/local/TitleBarView.tsx
+++ src/components/local/TitleBarView.tsx
@@ -9,19 +9,14 @@ const TitleBar = ({treeName}:{treeName?:string}) => {
   return (
     <div className='absolute top-0 left-0 text-white w-full px-8 py-4 flex items-center justify-between z-2'>
         <div className='text-3xl font-bold flex items-center justify-center gap-8'>
-            <div className='bg-[#00ff00] rounded-full text-black cursor-pointer hover:bg-[#00ff00a1]' onClick={()=>router.back()}><ChevronLeft size={40}/></div>
+            <div className='bg-[#00ff00] rounded-full text-black cursor-pointer hover:bg-[#00ff00a1]' onClick={()=>router.push("/")}><ChevronLeft size={40}/></div>
             {treeName}
         </div>
         <div>
             <SearchTree/>
         </div>
-        <div className='flex items-center justify-center gap-4'>
-            <div className='bg-[#00ff00] text-xl p-2 rounded-full cursor-pointer' >
-                <Share2 size={20} color='black'/>
-            </div>
-            <div className='bg-[#00ff00] text-xl p-2 rounded-full cursor-pointer' >
-                <Settings size={20} color='black'/>
-            </div>
+        <div className='bg-[#00ff00] text-xl p-2 rounded-full cursor-pointer' >
+            <Share2 size={20} color='black'/>
         </div>
     </div>
   )
